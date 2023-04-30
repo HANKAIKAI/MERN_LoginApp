aca7400adb0503081d9b82c6b8ea25d5e488b8ce
Kevin Han <yhan893071@gmail.com>
2023-04-30 12:11:16 -0400

    update __REACT_APP_BASE_URL__

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    __REACT_APP_BASE_URL__: `"${process.env.REACT_APP_BASE_URL}"`,
+    //__REACT_APP_BASE_URL__: `"${process.env.REACT_APP_BASE_URL}"`,
+    __REACT_APP_BASE_URL__: "http://127.0.0.1:8080",
   },
   plugins: [react()],
 });
